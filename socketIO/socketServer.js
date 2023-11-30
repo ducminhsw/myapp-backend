81ccc08046765fdba7c77f679ca21ed366e157bb
ducminhsw <119603933+ducminhsw@users.noreply.github.com>
2023-11-30 13:54:46 +0700

    update socket

--- socketIO/socketServer.js
+++ socketIO/socketServer.js
@@ -9,10 +9,48 @@ const registerSocketServer = (server) => {
         }
     });
 
-    ioServer.on('connect', socket => {
-        console.log('Socket on:', socket);
+    ioServer.on('connection', (socket) => {
+        console.log('Socket on');
 
-        socket.on('disconnect')
+        // webrtc
+        socket.on('server_signaling', (signal) => {
+            console.log('User create the answer.', signal);
+        });
+
+        socket.on('enter_room', (room) => {
+            console.log('User enter the room.', room);
+            socket.join(room.room);
+        });
+
+        socket.on('create_offer', (offer) => {
+            console.log('User create the offer.', offer);
+            socket.emit('receive_offer', { offer: offer });
+        });
+
+        socket.on('create_answer', (answer) => {
+            console.log('User create the answer.', answer);
+        });
+
+        socket.on('left_room', () => {
+            console.log('User left the room.');
+        });
+
+        // messages
+        socket.on('send-message', (socket) => {
+
+        });
+
+        socket.on('withdraw-message', (socket) => {
+
+        });
+
+        socket.on('recieve-message', (socket) => {
+
+        });
+
+        socket.on('disconnect', () => {
+
+        });
     });
 };
 
